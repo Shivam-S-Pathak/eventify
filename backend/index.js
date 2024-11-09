ff06fa27c80ce8e39d6bed6646fadef4c5ed7ff7
sinnilay <singhnilay55@gmail.com>
2024-11-09 21:26:12 +0530

    backend server add

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const connection= require("./DB_cannection")
+const cors = require('cors')
+const STUDENT_ROUTES=require("./Router/Student_Router")
+
+const app = express()
+const PORT = 8005
+const Db_URI="mongodb+srv://shivampathak2100:F9oUD0VPNY356Cdf@cluster0.zbsnz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+connection(Db_URI)
+.then(()=>console.log("DB CONNECTED ........"))
+.catch((e)=> console.log("ERROR: ",e))
+app.use(cors())
+app.use(express.json())
+app.use(cors())
+app.get("/test",(req,res)=>{
+    res.json({
+        msg:"HEELO CODER DO NOT WORRY SERVER IS RUNNING FINE......"
+    })
+})
+app.use("/api/student",STUDENT_ROUTES)
+app.listen(PORT,()=>{
+    console.log("SERVOR IS UP AT PORT NUMBER 8005");
+})
