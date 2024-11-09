5a13d835d1c1ced967d9dbdd725f8e7f171a0635
Shivam Pathak <shivampathak5042.com>
2024-11-09 22:52:06 +0530

    baar baar mt punch bhai 500 error aa rahi hai

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,28 @@
-const express = require('express')
-const connection= require("./DB_cannection")
-const cors = require('cors')
-const STUDENT_ROUTES=require("./Router/Student_Router")
+const express = require("express");
+const connection = require("./DB_cannection");
+const cors = require("cors");
+const STUDENT_ROUTES = require("./Router/Student_Router");
 
-const app = express()
-const PORT = 8005
-const Db_URI="mongodb+srv://shivampathak2100:F9oUD0VPNY356Cdf@cluster0.zbsnz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const app = express();
+const Db_URI =
+  "mongodb+srv://shivampathak2100:F9oUD0VPNY356Cdf@cluster0.zbsnz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 connection(Db_URI)
-.then(()=>console.log("DB CONNECTED ........"))
-.catch((e)=> console.log("ERROR: ",e))
-app.use(cors())
-app.use(express.json())
-app.get("/",(req,res)=>{
-    res.json({
-        msg:"HEELO CODER DO NOT WORRY SERVER IS RUNNING FINE......"
-    })
-})
-app.use("/api/student",STUDENT_ROUTES)
-app.listen(PORT,()=>{
-    console.log("SERVOR IS UP AT PORT NUMBER 8005");
-})
+  .then(() => console.log("DB CONNECTED ........"))
+  .catch((e) => console.log("ERROR: ", e));
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.json({
+    msg: "HELLO CODER! SERVER IS RUNNING FINE......",
+  });
+});
+
+
+app.use("/api/student", STUDENT_ROUTES);
+
+module.exports = app;
